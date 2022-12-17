73ccfa65c1c29c79b954877c5e96b9eb684f27fe
Pranit <pranitdubal5@gmail.com>
2022-12-17 20:39:50 +0530

    Theatre Added

---
+++ Controllers/threatre.controller.js
@@ -0,0 +1,32 @@
+const Theatre = require("../Models/theatre");
+
+exports.theatreCreation = async(req, res) => {
+    const theatreObj = {
+        name: req.body.name,
+        description: req.body.description,
+        city: req.body.city,
+        pincode: req.body.pincode,
+    }
+
+    try {
+        const theatre = await Theatre.create(theatreObj);
+        res.status(201).send(theatre)
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({
+            message: "Error in theatre creeation"
+        })
+    }
+}
+
+exports.getAllMovies = async(req, res) => {
+    try {
+        const theatres = await Theatre.find();
+        res.status(200).send(theatres)
+    } catch (error) {
+        console.log(error);
+        return res.status(500).send({
+            message: "Error in Finding theatres!"
+        })
+    }
+}
