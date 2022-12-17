8545657761101e0bc2b428b6551e761e0d8b1082
VishalDevelooperengineer <vk4993102@gmail.com>
2022-12-17 21:57:21 +0530

    theater update controller done

--- controllers/theater.controller.js
+++ controllers/theater.controller.js
@@ -1,75 +1,113 @@
 const THEATER = require('../models/theater.model');
 const fakeTheater = require('../seeders/theater.seed')
 
-exports.createTheater = async (req,res)=>{
+exports.createTheater = async (req, res) => {
     const body = req.body;
     const reqData = {
-        name:body.name,
-        description:body.description,
-        city:body.city,
-        pincode:body.pincode
+        name: body.name,
+        description: body.description,
+        city: body.city,
+        pincode: body.pincode
     }
     try {
-           const theater = await THEATER.create(reqData)
-           return res.status(201).send({
-            message:"Theater created successfully!"
-           })
-    }catch(err){
+        const theater = await THEATER.create(reqData)
+        return res.status(201).send({
+            message: "Theater created successfully!",
+            Created_Theater: theater
+        })
+    } catch (err) {
         console.log(err.message)
         return res.status(500).send({
-            message:"Internal server error!"
+            message: "Internal server error!"
         })
     }
 }
-exports.getTheaterById = async (req,res)=>{
-         const id = req.params.id
-         if(!id){
-            return  res.status(400).send({
-                message:"Id not found!"
+exports.getTheaterById = async (req, res) => {
+    const id = req.params.id
+    if (!id) {
+        return res.status(400).send({
+            message: "Id not found!"
+        })
+    }
+    try {
+        const theater = await THEATER.findOne({
+            _id: id
+        })
+        if (!theater) {
+            return res.status(400).send({
+                message: "theater does not exists!"
             })
-         }
-         try{
-            const theater = await THEATER.findOne({_id:id})
-            if(!theater){
-               return  res.status(400).send({
-                    message:"theater does not exists!"
-                })
-            }
-            return  res.status(201).send({
-                Theater:theater
-            })
-         }catch(err){
-            console.log(err.message)
-            return  res.status(500).send({
-                message:"Internal server error!"
-            })
-         }
+        }
+        return res.status(201).send({
+            Theater: theater
+        })
+    } catch (err) {
+        console.log(err.message)
+        return res.status(500).send({
+            message: "Internal server error!"
+        })
+    }
 }
-exports.getTheaterByAllFileds = async (req,res)=>{
+exports.getTheaterByAllFileds = async (req, res) => {
     const query = req.query
     const queryData = {}
-    if(name){
-         queryData.name = query.name
+    if (name) {
+        queryData.name = query.name
     }
-    if(city){
-        queryData.city = query,city
+    if (city) {
+        queryData.city = query, city
     }
-    if(pincode){
+    if (pincode) {
         queryData.pincode = query.pincode
     }
-    try{
-       const theater = await THEATER.findOne(queryData)
-       return  res.status(201).send({
-           Theater:theater
-       })
-    }catch(err){
-       console.log(err.message)
-       return  res.status(500).send({
-           message:"Internal server error!"
-       })
+    try {
+        const theater = await THEATER.findOne(queryData)
+        return res.status(201).send({
+            Theater: theater
+        })
+    } catch (err) {
+        console.log(err.message)
+        return res.status(500).send({
+            message: "Internal server error!"
+        })
     }
 }
-
+exports.updateTheater = async (req, res) => {
+    const body = req.body
+    const id = req.params.id
+    const reqData = {}
+    if (body.name) {
+        reqData.name = body.name
+    }
+    if (body.description) {
+        reqData.description = body.description
+    }
+    if (body.city) {
+        reqData.city = body.city
+    }
+    if (body.pincode) {
+        reqData.pincode = body.pincode
+    }
+    try {
+        const UpdatedTheater = await THEATER.updateOne({
+            _id: id
+        }, reqData)
+        if (!UpdatedTheater) {
+            return res.status(400).send({
+                message: "Theater  does not exists!",
+            })
+        }
+        return res.status(200).send({
+            message: "Theater updated successfully!",
+            Updated_Theater: UpdatedTheater
+        })
+    } catch (err) {
+        console.log(err.message)
+        return res.status(500).send({
+            message: "Internal server error!"
+        })
+    }
+}
 const createFakeTheater = async (data) => {
     for (let i = 0; i < data.length; i++) {
         await THEATER.create(data[i])
