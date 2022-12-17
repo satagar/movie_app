826d56a1beba968fb1daa41d7bef6f72d75d54d7
vim22al <vim22al@gmal.com>
2022-12-17 20:48:41 +0530

    done

---
+++ models/theatre.model.js
@@ -0,0 +1,36 @@
+const mongoose=require('mongoose');
+
+const theatreSchema= mongoose.Schema({
+
+    name:{
+        type:String,
+        required:true
+    },
+    description:{
+        type:String,
+        required:true
+    },
+    city:{
+        type:String,
+        required:true
+    },
+    pinCode:{
+        type:Number,
+        required:true
+    },
+    createdAt:{
+        type:String,
+        default:()=>{
+            return Date.now();
+        },
+        immutable: true
+    },
+    updatededAt:{
+        type:String,
+        default:()=>{
+            return Date.now();
+        }
+    }
+})
+
+module.exports=mongoose.model('theatre', theatreSchema)
