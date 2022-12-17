25f7dfe4eab72ec73840d8fca53f84d16c0542d8
abhigyanpatek <abhigyanpatek@gmail.com>
2022-12-17 20:26:45 +0530

    added some apis for movie

--- routes/movie.routes.js
+++ routes/movie.routes.js
@@ -5,5 +5,8 @@ const router = express.Router();
 
 router.post("/mba/api/v1/movies", validateMovieReqBody,movieController.createMovie);
 router.get("/mba/api/v1/movies", movieController.getAllMovies);
+router.get("/mba/api/v1/movies/:id", movieController.getMovieById);
+router.put("/mba/api/v1/movies/:id", movieController.updateMovie);
+router.delete("/mba/api/v1/movies/:id", movieController.deleteMovie);
 
 module.exports = router;
