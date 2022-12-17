2262a343e3475e79635390ff0db07173e2dffb1b
Pranit <pranitdubal5@gmail.com>
2022-12-17 22:36:49 +0530

    delete

--- Controllers/theatre.controller.js
+++ Controllers/theatre.controller.js
@@ -95,7 +95,7 @@ exports.delete = async(req, res) => {
                 message: 'Deleted Theatre Details Successfully'
             })
         } else {
-            res.status(404).send({
+            return res.status(404).send({
                 message: "Theatre Not Found!"
             })
         }
