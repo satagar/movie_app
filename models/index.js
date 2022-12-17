26d9fc20ac3d549bc9c6d7bb3bb7a572c614d839
samiksengupta <samiksengupta@hotmail.com>
2022-12-17 20:56:50 +0530

    fix theater model not exported

--- models/index.js
+++ models/index.js
@@ -4,11 +4,13 @@ const movie = require("./movie");
 const genre = require("./genre");
 const crew = require("./crew");
 const review = require("./review");
+const theater = require("./theater");
 
 module.exports = {
     User: user,
     Movie: movie,
     Genre: genre,
     Crew: crew,
-    Review: review
+    Review: review,
+    Theater: theater
 }
