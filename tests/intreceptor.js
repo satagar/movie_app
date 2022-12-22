7e469255c8139ad5378fa78b7def712ac4ec3607
Pranit <pranitdubal5@gmail.com>
2022-12-22 22:16:59 +0530

    tests

---
+++ tests/intreceptor.js
@@ -0,0 +1,14 @@
+exports.mockRequest = () => {
+    const req = {};
+    req.body = jest.fn().mockReturnValue(req);
+    req.params = jest.fn().mockReturnValue(req);
+    return req;
+}
+
+exports.mockResponse = () => {
+    const res = {};
+    res.status = jest.fn().mockReturnValue(res);
+    res.send = jest.fn().mockReturnValue(res);
+    res.json = jest.fn().mockReturnValue(res);
+    return res;
+}
