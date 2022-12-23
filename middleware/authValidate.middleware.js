56ae47455618ed520f31cbf92a5f55e7556df7b5
VishalDevelooperengineer <vk4993102@gmail.com>
2022-12-23 20:51:41 +0530

    change auth middleware

--- middleware/authValidate.middleware.js
+++ middleware/authValidate.middleware.js
@@ -2,29 +2,54 @@ const userTypeValidate = require('../utils/constant')
 const jwt = require('jsonwebtoken')
 const USER = require('../models/user.model')
 const key = require('../configs/scretKey')
-exports.authBodyValidate = (req, res, next) => {
+const isValidEmail = (email)=>{
+    return String(email).toLowerCase().match(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1, 3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+}
+exports.authBodyValidate =async  (req, res, next) => {
     const body = req.body;
     if (!body.name || !body.email || !body.password) {
         return res.status(400).send({
             message: "Bad request!"
         })
     }
+    if(body.email){
+        if(!isValidEmail(body.email)){
+            return res.status(400).send({
+                message: "Invalid Email , Bad request."
+            })
+        }
+         const user = await USER.findOne({email:body.email});
+         if(user){
+            return res.status(400).send({
+                message: "email already exists Please fill unique email , Bad request!"
+            })
+         }
+    }
+
     if(body.userType){
         if(!userTypeValidate.usertype[body.userType.toUpperCase()]){
             return res.status(400).send({
-                message: "Invalied userType ,  Bad Request."
+                message: "Invalid userType ,  Bad Request."
             })
         }
         body.userType = userTypeValidate.usertype[body.userType.toUpperCase()];
     }
     next()
 }
+
 exports.isValidBodyForSignin = (req,res,next)=>{
     const body = req.body
     if (!body.email || !body.password) {
         return res.status(400).send({
             message: "Bad request!"
         })
+    }
+    if(body.email){
+        if(!isValidEmail(body.email)){
+            return res.status(400).send({
+                message: "Invalied Email , Bad request."
+            })
+        }
     }
     next()
 }
