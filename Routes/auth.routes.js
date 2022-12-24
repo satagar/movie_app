ece6e0f766e0e6c24b54cf030bb59d1967d587fb
Pranit <pranitdubal5@gmail.com>
2022-12-24 21:33:05 +0530

    theatre deleteByName

--- Routes/auth.routes.js
+++ Routes/auth.routes.js
@@ -16,25 +16,25 @@ const theatreMovieController = require("../Controllers/theatreMovie.controller")
 //-------------------------User ------------------------------------
 router.post("/movie_app/api/v1/auth/signup", UserMiddleware.verifyUserRequest, authController.signup);
 router.post("/movie_app/api/v1/auth/login", authController.login);
-router.put("/movie_app/api/v1/users", validate.validateToken, UpdateMiddleware.verifyUserStatus, updateController.update)
-router.put("/movie_app/api/v1/users/:userId", validate.validateToken, isAdminvalidation.isAdmin, UpdateMiddleware.verifyUserStatus, updateController.userUpdate)
+router.put("/movie_app/api/v1/users", validate.validateToken, isAdminvalidation.isAdmin, UpdateMiddleware.verifyUserStatus, updateController.update)
+router.put("/movie_app/api/v1/users/:userId", validate.validateToken, isAdminvalidation.isAdmin, isAdminvalidation.isAdmin, UpdateMiddleware.verifyUserStatus, updateController.userUpdate)
 
 //--------------------Movie routes ---------------------------------
-router.post("/movie_app/api/v1/movies", validateMovie.movieValidate, movieController.movieCreation);
-router.get("/movie_app/api/v1/movies", movieController.getAllMovies);
-router.get("/movie_app/api/v1/movies/:id", movieController.getById); //http://localhost:5500/movie_app/api/v1/movies/639dd490c7a8f38a0572e033
-router.put("/movie_app/api/v1/movies/:id", validateMovie.movieValidate, movieController.update);
-router.delete("/movie_app/api/v1/movies/:id", movieController.delete);
+router.post("/movie_app/api/v1/movies", validate.validateToken, isAdminvalidation.isAdmin, validateMovie.movieValidate, movieController.movieCreation);
+router.get("/movie_app/api/v1/movies", validate.validateToken, movieController.getAllMovies);
+router.get("/movie_app/api/v1/movies/:id", validate.validateToken, movieController.getById); //http://localhost:5500/movie_app/api/v1/movies/639dd490c7a8f38a0572e033
+router.put("/movie_app/api/v1/movies/:id", validate.validateToken, isAdminvalidation.isAdmin, validateMovie.movieValidate, movieController.update);
+router.delete("/movie_app/api/v1/movies/:id", validate.validateToken, isAdminvalidation.isAdmin, movieController.delete);
 
 
 //---------------------Theatre routes-----------------------------
-router.post("/movie_app/api/v1/theatres", validate.validateToken, validateTheatre.theatreValidate, theatreController.theatreCreation);
+router.post("/movie_app/api/v1/theatres", validate.validateToken, isAdminvalidation.isAdmin, validateTheatre.theatreValidate, theatreController.theatreCreation);
 router.get("/movie_app/api/v1/theatres", validate.validateToken, theatreController.getAlltheatres);
 router.get("/movie_app/api/v1/theatres/:id", validate.validateToken, theatreController.getById);
 router.get("/movie_app/api/v1/theatres/:pincode", validate.validateToken, theatreController.getByPincode);
 router.get("/movie_app/api/v1/theatres/:city", validate.validateToken, theatreController.getByCity);
-router.put("/movie_app/api/v1/theatres/:id", validate.validateToken, validateTheatre.theatreValidate, theatreController.update);
-router.delete("/movie_app/api/v1/theatres/:id", validate.validateToken, theatreController.delete);
+router.put("/movie_app/api/v1/theatres/:id", validate.validateToken, isAdminvalidation.isAdmin, validateTheatre.theatreValidate, theatreController.update);
+router.delete("/movie_app/api/v1/theatres/:id", validate.validateToken, isAdminvalidation.isAdmin, theatreController.delete);
 
 
 
