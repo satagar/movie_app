201e0f5f77d192be007bdd0c9d48b72f18504dfa
Rahul <raahull24041978@gmail.com>
2022-12-24 22:12:06 +0530

    isAdmin check on routes

--- routes/theatre.routes.js
+++ routes/theatre.routes.js
@@ -1,11 +1,12 @@
-const theatreController = require("../controllers/theatre.controller");
+const theatreController = require("../controllers/theatre.controller")
+const authJwt = require('../middlewares/user.validator')
 
 module.exports = function (app) {
-    app.post("/mba/api/v1/theatres", theatreController.createTheatre);
-    app.get("/mba/api/v1/theatres", theatreController.getAllTheatres);
-    app.get("/mba/api/v1/theatres/:id", theatreController.getTheatre);
-    app.put("/mba/api/v1/theatres/:id", theatreController.updateTheatre);
-    app.delete("/mba/api/v1/theatres/:id", theatreController.deleteTheatre);
-    app.put("/mba/api/v1/theatres/:id/movies", theatreController.addMoviesToATheater); 
-    app.get("/mba/api/v1/theatres/:theatreId/movies/:movieId", theatreController.checkMovieInsideATheatre);
+    app.post("/mba/api/v1/theatres", [authJwt.isAdmin], theatreController.createTheatre)
+    app.get("/mba/api/v1/theatres", theatreController.getAllTheatres)
+    app.get("/mba/api/v1/theatres/:id", theatreController.getTheatre)
+    app.put("/mba/api/v1/theatres/:id", [authJwt.isAdmin], theatreController.updateTheatre)
+    app.delete("/mba/api/v1/theatres/:id", [authJwt.isAdmin], theatreController.deleteTheatre)
+    app.put("/mba/api/v1/theatres/:id/movies", [authJwt.isAdmin], theatreController.addMoviesToATheater)
+    app.get("/mba/api/v1/theatres/:theatreId/movies/:movieId", theatreController.checkMovieInsideATheatre)
 }
