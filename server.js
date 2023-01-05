f139d76b9fd1d592669b619f5989b016249a946d
VishalDevelooperengineer <vk4993102@gmail.com>
2023-01-06 00:07:02 +0530

    changes

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 const mongoose  = require('mongoose');
 const app = require('./index')
-const mongodbUrl = require('./configs/scretkey')
 require('dotenv').config()
 // process.env.MONGO_URI+process.env.DB_NAME
 mongoose.set('strictQuery', true);
-mongoose.connect(mongodbUrl.production_DB_URL,{family:4},(err)=>{
+mongoose.connect("mongodb+srv://movie-app-db:movieApp@movie-app-db.a09aeau.mongodb.net/?retryWrites=true&w=majority",{family:4},(err)=>{
           if(!err){
             console.log('Node envirnment : ', process.env.NODE_ENV)
             console.log("database connected successfully DataBaseName : ", process.env.DB_NAME);
